88d179abfec2c56d62e2f5c1e1998f686d15b144
Leith Mahfoudhi <76526645+Leith-mh@users.noreply.github.com>
2022-12-08 20:45:31 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,6 +8,23 @@ def buildImage() {
     }
 }
 
+def cloneCode() {
+   //cloning code 
+// The below will clone your repo and will be checked out to master branch by default.
+           git credentialsId: 'github', url: 'https://github.com/Leith-mh/SpringBoot-API-CICD.git'
+           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
+           sh "ls -lart ./*" 
+           // List all branches in your repo. 
+           sh "git branch -a"
+           // Checkout to a specific branch in your repo.
+           sh "git checkout master"
+} 
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn -f ./pom.xml clean package'
+    sh 'mvn -version'
+} 
+
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
     sh 'mvn clean deploy -Dmaven.test.skip=true'
